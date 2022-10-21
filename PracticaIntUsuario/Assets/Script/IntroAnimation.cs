a3041c6b303b9e2679538752c5d7d8925099256f
Dxvah <idayraperezdelgado@gmail.com>
2022-10-21 13:47:28 +0100

    Falta la transición al menú principal

---
+++ PracticaIntUsuario/Assets/Script/IntroAnimation.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroAnimation : MonoBehaviour
+{
+    [SerializeField]
+    GameObject logo;
+
+
+    [SerializeField]
+    GameObject interfaz;
+
+    void Start()
+    {
+        //Oculto mi logo de manera inmediata, en cero segundos
+        LeanTween.moveLocalY(logo, -331f, 0.0f);
+        //Hago aparecer el logo desde su posición inicial hacia la posicion 52 del eje Y
+        LeanTween.moveLocalY(logo, 52f, 7f).setEaseInOutQuint().setOnComplete(Latido);
+        //LeanTween.moveLocal(logo, new Vecto3(xf, yf, zf), tiempo f) - para moverlo en diversos ejes al mismo tiempo 
+
+
+    }
+
+
+    void Latido()
+    {
+
+
+        LeanTween.scale(logo, Vector3.one, 1f).setEaseOutBack().setOnComplete(() =>
+       {
+
+           LeanTween.alphaCanvas(gameObject.GetComponent<CanvasGroup>(), 0.0f, 0.75f).setOnComplete(Desactivar);
+
+       });
+
+        void Desactivar()
+        {
+            gameObject.SetActive(false);
+            //Activar el menu principal
+
+        }
+
+    }
+
+    void OnEnable()
+    {
+        
+        LeanTween.moveLocalX(interfaz, -622f, 10f).setEaseInExpo().setOnComplete(Menu);
+
+    }
+
+
+    void Menu()
+    {
+
+        gameObject.SetActive(true);
+    }
+}
